168ffcc1d76f522d9dd4e9cc33a5a0fe3e9e6556
Sergei Golendukhin <golendukhin.s.001@gmail.com>
2023-09-07 18:54:27 +0500

    exception tested

---
+++ src/test/java/ProductManager/ShopRepositoryTest.java
@@ -0,0 +1,45 @@
+package ProductManager;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShopRepositoryTest {
+
+    @Test
+    public void testRemoveWhenProductExist (){
+        ShopRepository repo = new ShopRepository();
+        Product product1 = new Product(11, "Телефон", 30_000);
+        Product product2 = new Product(22, "Книга", 300);
+        Product product3 = new Product(300, "Машина", 3_000_000);
+
+        repo.add(product1);
+        repo.add(product2);
+        repo.add(product3);
+        repo.remove(22);
+
+        Product[] actual = repo.findAll();
+        Product[] expected = {product1, product3};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemoveWhenProductNotExist (){
+        ShopRepository repo = new ShopRepository();
+        Product product1 = new Product(11, "Телефон", 30_000);
+        Product product2 = new Product(22, "Книга", 300);
+        Product product3 = new Product(300, "Машина", 3_000_000);
+
+        repo.add(product1);
+        repo.add(product2);
+        repo.add(product3);
+
+        Assertions.assertThrows(NotFoundException.class,
+                () -> repo.remove(4444)
+                );
+    }
+
+
+}
